a16edd9ec3099eb4881aee46ea260a63380cfa22
alvinhui <alvin.hui@qq.com>
2017-12-17 11:52:50 +0800

    feat: simple naivebayes

--- 03_naive_bayes/naiveBayes.js
+++ 03_naive_bayes/naiveBayes.js
@@ -1,9 +1,15 @@
+const math = require('mathjs');
+
 function createList(length, number = 0) {
   const list = [];
   for (const i = 0; length > i; i++) {
     list.push(number);
   }
   return list;
+}
+
+function addVec(v1, v2) {
+
 }
 
 function loadDataSet() {
@@ -52,12 +58,41 @@ function document2VecList(document) {
   return list;
 }
 
-function train(matrix, categories) {
+// 二分类训练
+function train(matrix, categories, debug = false) {
+  const numTrainDocs = matrix.length;
+  const numWords = matrix[0].length;
+  const pAbusive = 0.5;
+  let p0Num = math.zeros(numWords);
+  let p1Num = math.zeros(numWords);
+  let p0Denominator = 0.0, 
+    p1Denominator = 0.0;
 
+  for (let i = 0; (numTrainDocs - 1) > i; ++i) {
+    if (categories[i] === 1) {
+      p1Num = math.add(p1Num, matrix[i]);
+      p1Denominator += math.sum(matrix[i]);
+    } else {
+      p0Num = math.add(p0Num, matrix[i]);
+      p0Denominator += math.sum(matrix[i]);
+    }
+  }
+
+  const p1Vec = math.divide(p1Num, p1Denominator);
+  const p0Vec = math.divide(p0Num, p0Denominator);
+
+  return {
+    p1Vec,
+    p0Vec,
+    pAbusive
+  };
 }
 
-function classify() {
-
+// 二分类预测
+function classify(vec2Classify, p0Vec, p1Vec, pClass1) {
+  const p1 = math.sum(math.multiply(vec2Classify, p1Vec)) + math.log(pClass1);
+  const p0 = math.sum(math.multiply(vec2Classify, p0Vec)) + math.log(1.0 - pClass1);
+  return p1 > p0 ? 1 : 0;
 }
 
 module.exports = {
--- 03_naive_bayes/test.js
+++ 03_naive_bayes/test.js
@@ -1,7 +1,36 @@
-const { loadDataSet, createVocabList, words2Vec } = require('./naiveBayes');
+const { loadDataSet, createVocabList, words2Vec, train, classify } = require('./naiveBayes');
 
+// words2Vec
+// const {document, classVec} = loadDataSet();
+// const vocabList = createVocabList(document);
+// console.log(vocabList);
+// console.log(words2Vec(vocabList, document[0]));
+// console.log(words2Vec(vocabList, document[3]));
+
+// testingNB
 const {document, classVec} = loadDataSet();
 const vocabList = createVocabList(document);
-console.log(vocabList);
-console.log(words2Vec(vocabList, document[0]));
-console.log(words2Vec(vocabList, document[3]));
+const trainMat = [];
+
+for (const words of document) {
+  trainMat.push(words2Vec(vocabList, words));
+}
+
+console.log('trainMat', JSON.stringify(trainMat));
+console.log('classVec', classVec);
+
+const {p0Vec, p1Vec, pAbusive} = train(trainMat, classVec, true);
+
+console.log('p0Vec', JSON.stringify(p0Vec));
+console.log('p1Vec', JSON.stringify(p1Vec));
+console.log('pAbusive', pAbusive);
+
+const testEntry = ['love', 'my', 'dalmation'];
+const testVec = words2Vec(vocabList, testEntry);
+console.log('testEntry', testEntry);
+console.log('classified as:', classify(testVec, p0Vec, p1Vec, pAbusive));
+
+const testEntry2 = ['stupid', 'garbage'];
+const testVec2 = words2Vec(vocabList, testEntry2);
+console.log('testEntry2', testEntry2);
+console.log('classified as:', classify(testVec2, p0Vec, p1Vec, pAbusive));
