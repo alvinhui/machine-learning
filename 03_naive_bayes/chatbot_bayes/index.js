4f4755283680a93d4e837beee3b71e2e5f40cc90
alvinhui <alvin.hui@qq.com>
2018-05-10 14:26:38 +0800

    feat: 使用社区的 bayes 库进行测试

---
+++ 03_naive_bayes/chatbot_bayes/index.js
@@ -0,0 +1,25 @@
+// 使用社区的 bayes 库进行测试
+
+const bayes = require('bayes')
+const nodejieba = require('nodejieba');
+const { intents } = require('../chatbot/trainingData_chinese.json');
+const { listen } = require('../naiveBayes');
+
+const classifier = bayes({
+  tokenizer: (s) => nodejieba.cut(s)
+});
+
+intents.forEach(function(intent) {
+  const {patterns, tag} = intent;
+  patterns.forEach(function(pattern) {
+    classifier.learn(pattern, tag)
+  });
+});
+
+listen(function(text) {
+  if (text.length) {
+    console.log(classifier.categorize(text));
+  } else {
+    console.log('在干啥');
+  }
+})
