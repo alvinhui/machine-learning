ee59967c2e1ad0178737bcdb2f9d879e8b143f2a
alvinhui <alvin.hui@qq.com>
2018-05-14 09:23:10 +0800

    chore: init train

--- 04_logistic/chatbot/training.js
+++ 04_logistic/chatbot/training.js
@@ -1,2 +1,26 @@
 const {intents} = require('./trainingData_chinese.json');
 const nodejieba = require('nodejieba');
+const {tokenizer} = require('../utils');
+const word2vec = require('word2vec');
+
+w2v.loadModel(__dirname + '/vectors.txt', function( err, model ) {
+  const x = [];
+  const y = [];
+  const classes = [];
+  intents.forEach(function(intent) {
+    const {patterns, tag} = intent;
+    patterns.forEach(function(pattern) {
+      const words = tokenizer(pattern);
+
+      x.push(words);
+      y.push(tag);
+
+      if (classes.indexOf(tag) === -1) {
+        classes.push(tag);
+      }
+    });
+    responses[tag] = intent.responses;
+  });
+});
+
+
